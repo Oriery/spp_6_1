c0d6979af7e87cb7da1c17ead45e149b24f811db
Oriery <panev291002@gmail.com>
2023-02-06 17:47:32 +0300

    adjust logger

--- middlewares.js
+++ middlewares.js
@@ -1,9 +1,11 @@
+import c from 'colors'
+
 export function requestTime(req, res, next) {
   req.requestTime = Date.now()
   next()
 }
 
 export function logger(req, res, next) {
-  console.log(`Req.time: ${req.requestTime}`)
+  console.log(`${c.grey(new Date(req.requestTime).toLocaleString()+':')} ${c.green(req.method)} ${req.url}`)
   next()
 }
