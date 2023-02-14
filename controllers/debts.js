3a7f93f8a373fa1129ff456d5550302e1d9be7eb
Oriery <panev291002@gmail.com>
2023-02-14 23:38:45 +0300

    get debts from backend

--- controllers/debts.js
+++ controllers/debts.js
@@ -1,9 +1,10 @@
-let debts = [
-  {id: '1', name: 'Debt 1', person: "Dave", iOwn: true, amount: 1000, status: 'pending'},
-  {id: '2', name: 'Debt 2', person: "Mark", iOwn: false, amount: 2000, status: 'pending'},
-  {id: '3', name: 'Debt 3', person: "Julie", iOwn: true, amount: 3000, status: 'paid'},
-  {id: '4', name: 'Debt 4', person: "Andrew", iOwn: false, amount: 4000, status: 'paid'},
-]
+import axios from 'axios'
+
+const convertStatus = {
+  0: 'pending',
+  1: 'paid',
+  2: 'cancelled'
+}
 
 export const debtsPage = (req, res) => {
   renderDebtsPage(res)
@@ -11,21 +12,25 @@ export const debtsPage = (req, res) => {
 
 export const postDebt = (req, res) => {
   if (req.body.id) { setDebtStatus(req, res); return }
-  
+
   const newDebt = {
     id: Date.now().toString(),
     status: 'pending',
-    ...req.body,
+    iOwe: req.body.iOwe === 'true',
+    name: req.body.name,
+    person: req.body.person,
+    amount: req.body.amount
   }
-  debts.push(newDebt)
+  
+  axios.post('http://localhost:4004/user/Debts', newDebt)
 
   renderDebtsPage(res)
 }
 
-function setDebtStatus (req, res) {
+async function setDebtStatus (req, res) {
   const {status, id} = req.body
 
-  const debt = debts.find((debt) => debt.id == id)
+  const debt = await axios.get(`http://localhost:4004/user/Debts/${id}`)
 
   if (debt) {
     debt.status = status
@@ -35,6 +40,22 @@ function setDebtStatus (req, res) {
   }
 } 
 
-function renderDebtsPage(res, error) {
-  res.render('debts', {title: 'Debts', active: 'debts', debts: debts, error : error})
+async function renderDebtsPage(res, error) {
+  res.render('debts', {title: 'Debts', active: 'debts', debts: await getDebts(), error : error})
+}
+
+async function getDebts() {
+  const response = await axios.get('http://localhost:4004/user/Debts')
+
+  let debts = response.data.value
+  // convert to right format
+  debts = debts.map(debt => {
+    debt.id = debt.ID,
+    debt.amount = debt.initAmount
+    debt.status = convertStatus[debt.status]
+
+    return debt
+  })
+
+  return debts
 }
