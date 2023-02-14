e2d4ed5495e92c965787a711fa9e0ffb57755c07
Oriery <panev291002@gmail.com>
2023-02-14 21:57:38 +0300

    add error message; refactor routes and responses

--- controllers/debts.js
+++ controllers/debts.js
@@ -1,34 +1,40 @@
 let debts = [
-  {id: '1', name: 'Debt 1', amount: 1000, status: 'pending'},
-  {id: '2', name: 'Debt 2', amount: 2000, status: 'pending'},
-  {id: '3', name: 'Debt 3', amount: 3000, status: 'paid'},
-  {id: '4', name: 'Debt 4', amount: 4000, status: 'paid'},
+  {id: '1', name: 'Debt 1', person: "Dave", iOwn: true, amount: 1000, status: 'pending'},
+  {id: '2', name: 'Debt 2', person: "Mark", iOwn: false, amount: 2000, status: 'pending'},
+  {id: '3', name: 'Debt 3', person: "Julie", iOwn: true, amount: 3000, status: 'paid'},
+  {id: '4', name: 'Debt 4', person: "Andrew", iOwn: false, amount: 4000, status: 'paid'},
 ]
 
 export const debtsPage = (req, res) => {
-  res.render('debts', {title: 'Debts', active: 'debts', debts: debts})
+  renderDebtsPage(res)
 }
 
-export const createDebt = (req, res) => {
+export const postDebt = (req, res) => {
+  if (req.body.id) { setDebtStatus(req, res); return }
+  
   const newDebt = {
     id: Date.now().toString(),
-    ...req.body
+    status: 'pending',
+    ...req.body,
   }
   debts.push(newDebt)
 
-  res.redirect('/debts')
+  renderDebtsPage(res)
 }
 
-export const setDebtStatus = (req, res) => {
-  const {status} = req.body
-  const {id} = req.params
+function setDebtStatus (req, res) {
+  const {status, id} = req.body
 
   const debt = debts.find((debt) => debt.id == id)
 
   if (debt) {
     debt.status = status
-    res.redirect('/debts')
+    renderDebtsPage(res)
   } else {
-    res.status(404).json({message: 'Debt not found.'})
+    renderDebtsPage(res, {code: 404, message: 'Debt not found.'})
   }
 } 
+
+function renderDebtsPage(res, error) {
+  res.render('debts', {title: 'Debts', active: 'debts', debts: debts, error : error})
+}
--- routes/debts.js
+++ routes/debts.js
@@ -1,11 +1,9 @@
 import {Router} from 'express'
-import {createDebt, setDebtStatus, debtsPage} from '../controllers/debts.js'
+import {postDebt, debtsPage} from '../controllers/debts.js'
 const router = Router()
 
 router.get('/debts', debtsPage)
 
-router.post('/debts', createDebt)
-
-router.post('/debts/:id', setDebtStatus)
+router.post('/debts', postDebt)
 
 export default router
